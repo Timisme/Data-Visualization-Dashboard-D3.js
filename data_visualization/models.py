0704a6c9e0a0fd1070c7bb20532409a30f33a81d
Timisme <tom870315@gmail.com>
2024-02-28 23:03:07 +0800

    update models according to the json data

--- data_visualization/models.py
+++ data_visualization/models.py
@@ -1,13 +1,21 @@
 # Create your models here.
 from django.db import models
 
-
 class DataPoint(models.Model):
-    intensity = models.FloatField()
-    likelihood = models.FloatField()
-    relevance = models.FloatField()
-    year = models.IntegerField()
-    country = models.CharField(max_length=100)
-    topics = models.CharField(max_length=200)
-    region = models.CharField(max_length=100)
-    city = models.CharField(max_length=100)
+    end_year = models.IntegerField(null=True)
+    intensity = models.IntegerField()
+    sector = models.CharField(max_length=64, null=True)
+    topic = models.CharField(max_length=64, null=True)
+    insight = models.CharField(max_length=128, null=True)
+    url = models.URLField()
+    region = models.CharField(max_length=64, null=True)
+    start_year = models.IntegerField(null=True)
+    impact = models.IntegerField(null=True)
+    added = models.DateTimeField()
+    published = models.DateTimeField(null=True)
+    country = models.CharField(max_length=128, null=True)
+    relevance = models.IntegerField(null=True)
+    pestle = models.CharField(max_length=64, null=True)
+    source = models.CharField(max_length=64, null=True)
+    title = models.CharField(max_length=256, null=True)
+    likelihood = models.IntegerField(null=True)
