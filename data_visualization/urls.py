6f5a6d47fdc8a65b96429f36f79798fc8859afd1
Timisme <tom870315@gmail.com>
2024-02-29 00:31:48 +0800

    hotfix: add updated files back (backend works properly for now)

--- data_visualization/urls.py
+++ data_visualization/urls.py
@@ -4,7 +4,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
     path('data-points/', views.DataPointList.as_view(), name='data-point-list'),
     path('api/data/', views.DataPointList.as_view(), name='data-list'),
 ]
